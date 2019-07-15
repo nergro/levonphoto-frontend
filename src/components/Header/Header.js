92b357fec36edf92c9cdf0b2b47d5957dc8b944b
Nerijus <nerius10@gmail.com>
2019-07-15 16:17:08 +0300

    Adding header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  state = {
+    mobileNavOpen: false
+  };
+
+  handleMobileNav = () => {
+    this.setState(prevState => {
+      return {
+        mobileNavOpen: !this.state.mobileNavOpen
+      };
+    });
+  };
+
+  render() {
+    const mobileNavClasses = this.state.mobileNavOpen
+      ? 'mobile-nav mobile-nav-open'
+      : 'mobile-nav mobile-nav-close';
+    return (
+      <div className='header'>
+        <div className='header-bottom'>
+          <div className='header-bottom-wrapper'>
+            <div className='header-bottom-brand'>
+              <a href='##'>
+                <span className='regular'>LEVON</span>
+                <span className='highlighted'>photo</span>
+              </a>
+            </div>
+            <button className='toggle-button' onClick={this.handleMobileNav}>
+              <span className='toggle-button__bar' />
+              <span className='toggle-button__bar' />
+              <span className='toggle-button__bar' />
+            </button>
+            <nav className='header-bottom-nav'>
+              <ul className='header-bottom-nav__items'>
+                <li className='header-bottom-nav__item '>
+                  <a className='active' href='##'>
+                    PAGRINDINIS
+                  </a>
+                </li>
+                <li className='header-bottom-nav__item'>
+                  <a href='##'>PASLAUGOS</a>
+                </li>
+
+                <li className='header-bottom-nav__item'>
+                  <a href='##'>GALERIJA</a>
+                </li>
+                <li className='header-bottom-nav__item'>
+                  <a href='##'>KONTAKTAI</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <nav className={mobileNavClasses}>
+            <ul className='mobile-nav__items'>
+              <li className='mobile-nav__item'>
+                <a href='##'>PAGRINDINIS</a>
+              </li>
+              <li className='mobile-nav__item'>
+                <a href='##'>PASLAUGOS</a>
+              </li>
+              <li className='mobile-nav__item'>
+                <a href='##'>GALERIJA</a>
+              </li>
+              <li className='mobile-nav__item'>
+                <a href='##'>KONTAKTAI</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
